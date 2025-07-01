e6fc2ccc205479859345c97c4d043455b161fd4c
KostasToulis <toulisk@gmail.com>
2025-07-01 11:26:59 +0300

    Extract best solutions

---
+++ extract_best_sol.py
@@ -0,0 +1,36 @@
+import os
+import re
+
+directory = 'FCVRP-Best Solutions'
+
+for filename in os.listdir(directory):
+    if filename.endswith('.txt'):
+        filepath = os.path.join(directory, filename)
+        with open(filepath, 'r') as f:
+            lines = [line.rstrip('\n') for line in f]
+
+        # Find all solution blocks
+        solutions = []
+        i = 0
+        while i < len(lines):
+            if lines[i].startswith('Cost:'):
+                cost = float(re.findall(r'Cost:\s*([0-9.]+)', lines[i])[0])
+                # Infer number of routes from the first solution
+                if not solutions:
+                    num_routes = 0
+                    j = i + 1
+                    while j < len(lines) and not lines[j].startswith('Cost:'):
+                        num_routes += 1
+                        j += 1
+                routes = lines[i+1:i+1+num_routes]
+                solutions.append((cost, [lines[i]] + routes))
+                i += 1 + num_routes
+            else:
+                i += 1
+
+        # Find the best solution
+        if solutions:
+            best = min(solutions, key=lambda x: x[0])
+            with open(filepath, 'w') as f:
+                for line in best[1]:
+                    f.write(line + '\n')
--- make_barplots.py
+++ make_barplots.py
@@ -2,17 +2,18 @@ import matplotlib.pyplot as plt
 
 
 def plot_solution_construction_method():
-    y = [92.37, 90.26, 92.89, 93.42, 93.95, 93.42]
-    x = ['Memory-based', 'Substitution-based', 'Equal probability', '75% Memory-based', '80% Memory-based', '90% Memory-based']
+    y = [92.37, 93.42, 93.95, 93.42, 92.89, 90.26]
+    x = ['Memory', '90% Memory', '80% Memory', '75% Memory',  'Equal probability', 'Substitution']
 
     # plt.bar(x, y, color='lightgray', edgecolor='black', width=0.7)
-    plt.bar(x, y, color=['#ff6961', '#ffe066', '#ff9999', '#ffcc99', '#ffb347', '#ffae42'], edgecolor='black', width=0.7)
-    plt.xlabel('Solution Construction Balance')
-    plt.ylabel('Solved (%)')
+    plt.bar(x, y, color=['#ff6961', '#ffe066', '#ff9999', '#ffcc99', '#ffb347', '#ff5733'], edgecolor='black', width=0.7)
+    plt.xlabel('Solution Reconstruction Balance', fontsize=18, labelpad=15)
+    plt.ylabel('% of instances solved or improved', fontsize=18)
     plt.ylim(89, 95)
     # plt.title('Comparison of Methods')
-    plt.xticks(rotation=30, ha='right')
-    plt.tight_layout()
+    plt.xticks(fontsize=16)
+    plt.yticks(fontsize=16)
+    plt.tight_layout(pad=0.2)
     plt.show()
 
 def plot_solution_population():
@@ -21,7 +22,7 @@ def plot_solution_population():
 
     plt.bar(x, y, color='lightgray', edgecolor='black', width=0.7)
     plt.xlabel('poolSize')
-    plt.ylabel('Solved (%)')
+    plt.ylabel('% of instances solved or improved', fontsize=18)
     plt.ylim(92, 97)
     # plt.title('Comparison of Methods')
     # plt.xticks(rotation=30, ha='right')
@@ -41,35 +42,38 @@ def plot_chain_length():
     plt.show()
 
 def plot_g_probability():
-    y = [86.2, 92.7, 96.6, 96.1]
-    x = ['0', '0.2', '0.33', '0.5']
-    plt.bar(x, y, color=['#ff6961', '#ff9999', '#ffcc99' , '#ffe066'], edgecolor='black', width=0.7)
-    plt.xlabel('g probability')
-    plt.ylabel('Solved (%)')
-    plt.ylim(85, 98)
-    plt.xticks(x)
-    plt.tight_layout()
+    y = [86.2, 92.7, 96.6, 96.1, 91.1]
+    x = ['0', '0.2', '0.33', '0.5', '1']
+    plt.bar(x, y, color=['#ff6961', '#ff9999', '#ffcc99', '#ffe066', 'orange'], edgecolor='black', width=0.7)
+    plt.xlabel('g', fontsize=16)
+    plt.ylabel('% of instances solved or improved', fontsize=18)
+    plt.ylim(75, 100)
+    plt.xticks(x, fontsize=16)
+    plt.yticks(fontsize=16)
     plt.show()
 
 def plot_line_g():
-    # In the g folder I have 4 txt files. Each line has two elements: the first is the solution cost (y-axis) and the second is the second (x-axis).
-    # Create a line plot for each file. The lines should be in different colors on the same chart. Additionally, label each line with the file name.
     import os
     import numpy as np
     g_folder = 'g'
     files = os.listdir(g_folder)
-    colors = ['#77dd77', '#84b6f4', '#cbaacb', '#f49ac2']
+    colors = ['#f49ac2', 'orange', 'yellow', 'red' ]
     plt.figure(figsize=(10, 6))
     for i, file in enumerate(files):
         if file.endswith('.txt'):
             data = np.loadtxt(os.path.join(g_folder, file), delimiter=',')
-            x = data[:, 1].astype(int)  # second column as int
-            y = data[:, 0].astype(int)  # first column as int  # first column
-            plt.plot(x, y, label=file[:-4], color=colors[i % len(colors)])  # remove .txt from label
-    plt.xlabel('Time (s)')
-    plt.ylabel('Solution Cost')
-    plt.title('Solution Cost Over Time')
-    plt.legend()
+            x = data[:, 1].astype(int)
+            y = data[:, 0].astype(int)
+            plt.plot(x, y, label=file[::-1].split('_', 1)[0][::-1].replace('.txt', ''), color=colors[i % len(colors)])
+    plt.ylim(430, 470)
+    plt.xlim(0, 400)
+    plt.xlabel('Time (s)', fontsize=16)
+    plt.ylabel('Solution Cost', fontsize=16)
+    plt.xticks(fontsize=16)
+    plt.yticks(fontsize=16)
+    # plt.title('Solution Cost Over Time')
+    plt.axhline(y=432, color='grey', linestyle='--', linewidth=1)
+    plt.legend(fontsize=22)
     plt.grid(True)
     plt.tight_layout()
     plt.show()
@@ -78,5 +82,5 @@ def plot_line_g():
 # plot_solution_construction_method()
 # plot_solution_population()
 # plot_chain_length()
-# plot_g_probability()
-plot_line_g()
+plot_g_probability()
+# plot_line_g()
