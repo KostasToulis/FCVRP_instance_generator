451481f496e2936a87a344fe0ed237f9d697e25d
KostasToulis <toulisk@gmail.com>
2025-07-17 11:42:03 +0300

    BARPLOT CHANGES

--- make_barplots.py
+++ make_barplots.py
@@ -5,14 +5,14 @@ def plot_solution_construction_method():
     # y = [92.37, 93.42, 93.95, 93.42, 92.89, 90.26]
     # y = [90.26, 92.89, 93.42, 93.95, 93.42, 92.37]
 
-    y = [94.27, 94.55, 94.01, 94.79, 95.05, 94.79, 94.27]
+    y = [93.75, 94.55, 94.01, 94.79, 95.05, 94.79, 94.27]
 
     x = ['0', '0.5', '0.6', '0.7', '0.8',  '0.9', '1']
 
     plt.grid(axis='y', linestyle='--', alpha=0.7, zorder=0)
     # plt.bar(x, y, color='lightgray', edgecolor='black', width=0.7)
     # plt.bar(x, y, color=['#ff6961', '#ffe066', '#ff9999', '#ffcc99', '#ffb347', '#ff5733'], edgecolor='black', width=0.7)
-    plt.bar(x, y, color=['lightgray', 'lightgray', 'lightgray', 'lightgray', 'skyblue', 'lightgray', 'lightgray'], edgecolor='black',
+    plt.bar(x, y, color=['lightgray', 'lightgray', 'lightgray', 'lightgray', 'tab:blue', 'lightgray', 'lightgray'], edgecolor='black',
             width=0.7, zorder=3)
     for i, v in enumerate(y):
         plt.text(i, v + 0.2, f"{v:.2f}", ha='center', fontsize=22, fontfamily='serif')
@@ -128,6 +128,7 @@ def plot_g_topArcs_heatmap():
     import seaborn as sns
 
     results = np.array([
+        [87.76, 87.76, 87.76, 87.76, 87.76, np.nan],
         [90.62, 90.62, 90.36, 91.93, 90.62, np.nan],
         [91.41, 91.41, 90.89, 91.15, 91.41, np.nan],
         [92.19, 93.23, 93.23, 93.23, 93.49, np.nan],
@@ -139,7 +140,7 @@ def plot_g_topArcs_heatmap():
     plt.figure(figsize=(8, 6))
     sns.heatmap(
         results[:, :6], annot=True, fmt=".2f", cmap="Blues", cbar=True,
-        yticklabels=[0.1, 0.2, 0.3, 0.4, 0.5, 1],
+        yticklabels=[0, 0.1, 0.2, 0.3, 0.4, 0.5, 1],
         xticklabels=['1%', '2%', '3%', '4%', '5%'],
         annot_kws={"fontsize": 20, "fontfamily": "serif"}
     )
@@ -150,10 +151,10 @@ def plot_g_topArcs_heatmap():
     plt.tight_layout()
     plt.show()
 
-plot_solution_construction_method()
+# plot_solution_construction_method()
 # plot_solution_population()
 # plot_chain_length()
 # plot_g_probability()
 # plot_top_arcs()
 # plot_line_g()
-# plot_g_topArcs_heatmap()
+plot_g_topArcs_heatmap()
